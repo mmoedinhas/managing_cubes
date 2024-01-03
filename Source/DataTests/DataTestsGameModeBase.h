a2d8017e54f2e28533b19faf6a839fb77f8e2fd1
Matilde Freilao <matilde.freilao@oceaninfinity.com>
2024-01-03 12:02:05 +0000

    Starter files for project

---
+++ Source/DataTests/DataTestsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DataTestsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DATATESTS_API ADataTestsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
